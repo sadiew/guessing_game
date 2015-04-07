3e99225d876e365efbea4afabd267bf1dfa038a0
Hackbright Student <info@hackbrightacademy.com>
2015-04-07 16:15:50 -0700

    added counter for # of tries

--- game.py
+++ game.py
@@ -5,15 +5,16 @@ print "I've got a game for you, %s" % name
 print "I'm thinking of a number between 1 and 100"
 guess = int(raw_input("Try to guess the number.\n"))
 our_number = random.randint(1, 100)
+counter = 0
 
-while guess <101:
-
+while guess in range(1,101):
+    counter += 1
     if guess < our_number:
         guess = int(raw_input("Too low. Try a little higher.\n"))
     elif guess > our_number:
         guess = int(raw_input("Too high. Try a little lower.\n"))
     else:
-        print "Congrats! You guessed correctly!\n"
+        print "Congrats! You guessed correctly! You found my number in %d tries\n" %counter
         break 
 
 
