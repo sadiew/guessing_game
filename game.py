f195da6b68ad6d5012a097b32ffe99bcf5689b7a
Hackbright Student <info@hackbrightacademy.com>
2015-04-07 16:31:14 -0700

    Made changes to simplify per Cynthia's advice.

--- game.py
+++ game.py
@@ -2,17 +2,18 @@ import random
 
 name = raw_input("Hey there partner, what's your name?\n")
 print "I've got a game for you, %s" % name
-print "I'm thinking of a number between 1 and 100"
-guess = int(raw_input("Try to guess the number.\n"))
+print "I'm thinking of a number between 1 and 100. Try to guess the number."
+
 our_number = random.randint(1, 100)
 counter = 0
 
-while guess in range(1,101):
+while True:
+    guess = int(raw_input("Your guess? "))
     counter += 1
     if guess < our_number:
-        guess = int(raw_input("Too low. Try a little higher.\n"))
+        print "Too low. Try a little higher."
     elif guess > our_number:
-        guess = int(raw_input("Too high. Try a little lower.\n"))
+        print "Too high. Try a little lower."
     else:
         print "Congrats! You guessed correctly! You found my number in %d tries\n" %counter
         break 
